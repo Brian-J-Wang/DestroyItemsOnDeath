f82a2316457654ed4a0e1de40ef288b8e8041a3f
Brian Wang <brianwang7102@gmail.com>
2025-11-18 06:11:15 -0500

    feat: implemented core drop item on death feature

--- src/server_scripts/onPlayerDeath.ts
+++ src/server_scripts/onPlayerDeath.ts
@@ -1,9 +1,10 @@
 EntityEvents.death(event => {
     if (!event.entity.isPlayer()) {
         return;
-    };
+    }
 
+    //@ts-ignore
     const player = (event.entity as Internal.Player);
-
-    player.inventory.allItems().remove();
-})
+    player.inventory.clearContent();
+    player.curiosInventory.equippedCurios.clear();      
+});
